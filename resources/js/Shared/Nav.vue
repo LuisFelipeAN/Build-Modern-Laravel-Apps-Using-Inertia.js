b0d3edd15d4cc849196295b877a103d473f54406
Luis Felipe <luis.felipe.almeida.nascimento@gmail.com>
2022-05-04 11:43:54 -0300

    episode

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,18 +1,20 @@
 <template>
     <nav class="mt-6">
         <ul class="list-disc">
-            <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-            <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-            <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
-             <li><Link href="/logout" method="post" as='button' :data="{foo : 'bar'}" class="text-blue-500 hover:underline">Log Out</Link></li>
+
+            <NavLink href="/" :active="$page.component==='Home'">Home </NavLink>
+            <NavLink href="/users" :active="$page.component==='Users'">Users </NavLink>
+            <NavLink href="/settings" :active="$page.component==='Settings'">Settings </NavLink>
+            <li><Link href="/logout" method="post" as='button' :data="{foo : 'bar'}" class="text-blue-500 hover:underline">Log Out</Link></li>
         </ul>
     </nav>
 </template>
 <script>
 import {Link} from '@inertiajs/inertia-vue3'
+import NavLink from './NavLink.vue'
 export default {
     components:{
-        Link
+        Link,NavLink
     },
 };
 </script>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <li><Link :href="href" class="text-blue-500 hover:underline" :class="{'font-bold underline': active}"><slot/></Link></li>    
+</template>
+<script>
+import {Link} from '@inertiajs/inertia-vue3'
+export default {
+    components:{
+        Link
+    },
+    props:{
+        href: String,
+        active: Boolean,
+    }
+};
+</script>
